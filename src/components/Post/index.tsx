5337d4b81d714ae72b8c39ce7a8c5bb28fdbebab
mwises <88902469+mwises@users.noreply.github.com>
2023-09-12 16:53:39 -0300

    Initial

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export const Post = () => {
+    return (
+        <div>
+            <h1>Post</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur molestiae non, voluptatum tempore quam ut assumenda optio dolorum! Tempore ea natus quis illum repellat facilis necessitatibus nostrum, fuga vero temporibus.</p>
+        </div>
+    );
+}
